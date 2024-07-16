a3e319264bee12c2694f65082f270620dc2e5c51
sugandhanjr <sudandhanjr@gmail.com>
2024-07-16 20:11:13 +0530

    error

---
+++ main.py
@@ -0,0 +1,10 @@
+
+from url_shortner import create_app
+
+
+app=create_app()
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
--- url_shortner/settings.py
+++ url_shortner/settings.py
@@ -1,6 +1,6 @@
 import os
 
-SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI =  'sqlite:///db.sqlite3'#os.environ.get('DATABASE_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 ADMIN_USERNAME = os.environ.get('ADMIN_USERNAME')
