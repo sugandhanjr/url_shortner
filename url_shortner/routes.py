8d826237d908d86ba678066231d9db1af36f95b8
sugandhanjr <sudandhanjr@gmail.com>
2024-07-17 12:55:54 +0530

    done

--- url_shortner/routes.py
+++ url_shortner/routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, redirect
 
 from .extensions import db
 from .models import Link
+from .auth import requires_auth
 
 short = Blueprint('short', __name__)
 
@@ -15,10 +16,12 @@ def redirect_to_url(short_url):
     return redirect(link.original_url)
 
 @short.route('/')
+@requires_auth
 def index():
     return render_template('index.html')
 
 @short.route('/add_link',methods=['POST'])
+@requires_auth
 def add_link():
     original_url = request.form['original_url']
     link = Link(original_url=original_url)
@@ -28,6 +31,7 @@ def add_link():
     return render_template('link_added.html',new_link=link.short_url, original_url=original_url )
 
 @short.route('/stats') 
+@requires_auth
 def stats():
     links = Link.query.all()
 
@@ -35,4 +39,5 @@ def stats():
 
 @short.errorhandler(404)
 def page_not_found(e):
-    return '', 404
+     return render_template('404.html'),404 
+
